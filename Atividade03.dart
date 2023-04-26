35d890708ec62ffdf46ef28f508eba26e9436604
Alan2277 <121243339+Alan2277@users.noreply.github.com>
2023-04-26 15:22:23 -0300

    Update Atividade03.dart

--- Atividade03.dart
+++ Atividade03.dart
@@ -56,13 +56,19 @@ class Agencia {
 }
 
 //Conta
+import 'Cliente.dart';
+import 'Agencia.dart';
 
 class Conta {
   String _numeroConta = "";
   double _saldo = 0.0;
+  Cliente _cliente = Cliente("", "");
+  Agencia _agencia = Agencia("", "");
 
-  Conta(String numero) {
+  Conta(String numero, Cliente cliente, Agencia agencia) {
     this.numeroConta = numero;
+    this._cliente = cliente;
+    this._agencia = agencia;
   }
 
   String get numeroConta {
@@ -73,8 +79,24 @@ class Conta {
     return this._saldo;
   }
 
-  void set numeroConta(String numero_Conta) {
+  Cliente get cliente {
+    return this._cliente;
+  }
+
+  Agencia get agencia {
+    return this._agencia;
+  }
+
+  set numeroConta(String numero_Conta) {
     this._numeroConta = numero_Conta;
+  }
+
+  set cliente(Cliente cliente) {
+    this._cliente = cliente;
+  }
+
+  set agencia(Agencia agencia) {
+    this._agencia = agencia;
   }
 
   void adicionar(double valor) {
@@ -86,24 +108,33 @@ class Conta {
   }
 }
 
-//Questão 03
+
+//Questão 04
 import 'Cliente.dart';
 import 'Agencia.dart';
 import 'Conta.dart';
 
+
 void main() {
-  Conta c1 = Conta("202-63");
-  Conta c2 = Conta("815-20");
-  Cliente cliente1 = Cliente("Alan", "233");
-  Cliente cliente2 = Cliente("Sampaio", "862");
+  Agencia centro = Agencia("41", "centro");
+  Cliente alan = Cliente("Alan", "52-89");
+  Cliente sampaio = Cliente("Sampaio", "14-02");
 
-  c1.adicionar(120.23);
-  c2.adicionar(32.87);
+  Conta conta1 = Conta("202", alan, centro);
+  Conta conta2 = Conta("815", sampaio, centro);
 
-  print(" PRIMEIRO CONTA\n Cliente: ${cliente1.nome}\n Número da Conta: ${c1.numeroConta}\n Saldo: ${c1.saldo}");
+  conta1.adicionar(120.23);
+  conta2.adicionar(32.87);
+
+  print(
+      " PRIMEIRO CONTA\n Cliente: ${alan.nome}\n Agencia: ${centro.numero}\n Banco: ${centro.nome}\n Número da Conta: ${conta1.numeroConta}\n Saldo: ${conta1.saldo}");
   print("------------------------------------------------------------------");
-  print(" SEGUNDA CONTA\n Cliente: ${cliente2.nome}\n Número da Conta: ${c2.numeroConta}\n Saldo: ${c2.saldo}");
+  print(
+      " SEGUNDA CONTA\n Cliente: ${conta1.cliente.nome}\n Agencia: ${centro.numero}\n Banco: ${centro.nome}\n Número da Conta: ${conta2.numeroConta}\n Saldo: ${conta2.saldo}");
+
+  print("${alan.codigo}");
 }
+
 
 //Questão 05
 import 'Cliente.dart';
